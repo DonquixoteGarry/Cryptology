1b37a070c29a6055e34ec6aa23b813ef1a4f3c37
Donquixotegarry <zhengji20001205@gmail.com>
2020-12-06 01:12:34 +0800

    lab3 some about main entry(aes.cpp)

--- lab3/code/aes.cpp
+++ lab3/code/aes.cpp
@@ -8,5 +8,41 @@ using namespace std;
 
 int main()
 {
-    
+    int order;
+    cout<<"\nplease input task code:  1-encrypt / 2-decrypt / 3-test-snow-fall / 4-quit";
+    while(cin>>order)
+    {
+        if(order==1)
+        {
+            cout<<"\nplease input plaintext(32-hex):\n";
+            string plaintext;
+            cin>>plaintext;
+            cout<<"please input key(32-hex):\n";
+            string aes_128_key;
+            cin>>aes_128_key;
+            //code...
+        }
+        if(order==2)
+        {
+            cout<<"\nplease input cryptedtext(32-hex):\n";
+            string cryptedtext;
+            cin>> cryptedtext;
+            cout<<"\nplease input key(32-hex):\n";
+            string aes_128_key;
+            cin>>aes_128_key;
+            //code...
+        }
+        if(order==3)
+        {
+            //code...
+        }
+        if(order==4)
+            return 0;
+        else
+        {
+            cout<<"\ntask code error";
+            cout<<"\n\n-----new task-----\n\n";
+        }
+    }
+
 }
